85af78885a15761663dea9eb4bf6e75fc653ff1b
Matias Arias <matiasarias3103@gmail.com>
2026-03-17 21:30:11 -0300

    Adding pool connection with HikaryCP

--- src/main/java/com/tasktracker/app/utils/ConnectionJdbc.java
+++ src/main/java/com/tasktracker/app/utils/ConnectionJdbc.java
@@ -1,23 +1,33 @@
 package com.tasktracker.app.utils;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
-import javax.sql.DataSource;
-import org.postgresql.ds.PGSimpleDataSource;
 
 /** Class that contain the logic to the JDBC connection. */
-public enum ConnectionJdbc {
-  INSTANCE;
-  private DataSource dataSource;
+public class ConnectionJdbc {
 
-  ConnectionJdbc() {
-    PGSimpleDataSource ds = new PGSimpleDataSource();
-    ds.setServerNames(new String[] {System.getenv("POSTGRES_HOST")});
-    ds.setDatabaseName(System.getenv("POSTGRES_DB"));
-    ds.setUser(System.getenv("POSTGRES_USER"));
-    ds.setPassword(System.getenv("POSTGRES_PASSWORD"));
-    this.dataSource = ds;
+  private static HikariConfig config = new HikariConfig();
+  private static HikariDataSource ds;
+
+  static {
+    config.setJdbcUrl(
+        "jdbc:postgresql://"
+            + System.getenv("POSTGRES_HOST")
+            + "localhost:5432/"
+            + System.getenv("POSTGRES_DB"));
+    config.setUsername(System.getenv("POSTGRES_USER"));
+    config.setPassword(System.getenv("POSTGRES_PASSWORD"));
+    config.setMaximumPoolSize(10);
+    config.setMinimumIdle(2);
+    config.setIdleTimeout(3000);
+    config.setConnectionTimeout(3000);
+    config.setLeakDetectionThreshold(1500);
+    ds = new HikariDataSource(config);
   }
+
+  private ConnectionJdbc() {}
 
   /**
    * Get a Connection for JDBC.
@@ -25,7 +35,7 @@ public enum ConnectionJdbc {
    * @return Connection for postgresql
    * @throws SQLException if can not connect to Posgresql
    */
-  public Connection getConnection() throws SQLException {
-    return dataSource.getConnection();
+  public static Connection getConnection() throws SQLException {
+    return ds.getConnection();
   }
 }
